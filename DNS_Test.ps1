24ea8c014ce9110926f793b8da3acbc6c0b860c6
DAVSCTDDSN <davsctddsn@hotmail.com>
2026-01-10 15:08:36 -0600

    Began Adding Error Correction

--- DNS_Test.ps1
+++ DNS_Test.ps1
@@ -1,20 +1,41 @@
 # Clear the Screen
 Clear-Host
 
+#Initialize the Variables
+$Result = @()
+$RecordResult = @()
+
 # List DNS Servers you want to query
 $DnsServers = "8.8.4.4", "8.8.8.8", "1.1.1.1"
 
 # List the DNS Records you want to query
-$DnsRecords = "yahoo.com", "msn.com", "google.com"
+$DnsRecords = "nbcdfw.com", "msn.com", "cbs11.com", "104.69.125.254", "myfakednsrecord.com"
 
 #Run the Query
 foreach ($Server in $DnsServers) {
     foreach ($Record in $DnsRecords){
+       try {
         $Result = ""
-        $Result =  resolve-dnsname $Record -Server $Server -DnsOnly
-        $Result | Add-Member -NotePropertyName "DNS_Server" -NotePropertyValue $Server
-        $RecordResult += $Result
+        $Result =  resolve-dnsname $Record -Server $Server -DnsOnly -ErrorAction Stop
+            $DetailedResult = New-Object System.Object
+            $DetailedResult | Add-Member -NotePropertyName "Record_Name" -NotePropertyValue $Record
+            $DetailedResult | Add-Member -NotePropertyName "Type" -NotePropertyValue $Result.QueryType
+            $DetailedResult | Add-Member -NotePropertyName "IP_Address" -NotePropertyValue $Result.IPAddress
+            $DetailedResult | Add-Member -NotePropertyName "DNS_Server" -NotePropertyValue $Server
+            $DetailedResult | Add-Member -NotePropertyName "Note" -NotePropertyValue "Test"
+        $RecordResult += $DetailedResult
+       }
+       catch {
+        $FailedResult = New-Object System.Object
+            $FailedResult   | Add-Member -NotePropertyName "Record_Name" -NotePropertyValue $Record
+            $FailedResult   | Add-Member -NotePropertyName "Type" -NotePropertyValue "."
+            $FailedResult | Add-Member -NotePropertyName "IP_Address" -NotePropertyValue "."
+            $FailedResult | Add-Member -NotePropertyName "DNS_Server" -NotePropertyValue $Server
+            $FailedResult | Add-Member -NotePropertyName "Note" -NotePropertyValue "Lookup Failed"
+       $RecordResult += $FailedResult
+       }
+        
+
     }
-    $ServerResults += $RecordResult
 }
-$ServerResults | select Name, DNS_Server, IPAddress, QueryType  | ft -AutoSize
+$RecordResult | Format-Table -AutoSize
